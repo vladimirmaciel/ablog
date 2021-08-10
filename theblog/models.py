83b368e4a7e08aa6813c7e2cbccfd525658a3c9d
Vladimir <vladimirmaciel@gmail.com>
2021-08-09 22:43:08 -0300

    implementação das aulas 22 e 23

--- theblog/models.py
+++ theblog/models.py
@@ -23,6 +23,7 @@ class Post(models.Model):
     # body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=255, default='coding')
+    snippet = models.CharField(max_length=255)
     likes = models.ManyToManyField(User, related_name='blog_post')
     
     def total_likes(self):
