d6f8e76b30054b12bbaff877bc0b08ab8b8a14b8
Vladimir <vladimirmaciel@gmail.com>
2021-08-17 18:25:16 -0300

    conclusao aulas 24 e 25

--- members/views.py
+++ members/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm, UserChangeForm
+from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from .forms import SignUpForm
+from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
 
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangingForm
+    # from_class = PasswordChangeForm
+    # success_url = reverse_lazy('home')
+    success_url = reverse_lazy('password_success')
+    
+    
+def password_success(request):
+    return render(request, 'registration/password_success.html', {})
+    
 
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
@@ -11,7 +23,7 @@ class UserRegisterView(generic.CreateView):
     success_url = reverse_lazy('login')
 
 class UserEditView(generic.UpdateView):
-    form_class = UserChangeForm
+    form_class = EditProfileForm
     template_name = 'registration/edit_profile.html'
     success_url = reverse_lazy('home')
     
