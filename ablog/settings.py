e63a6e3572b8259805d8f5db603b345018d7c821
Vladimir <vladimirmaciel@gmail.com>
2021-08-17 23:40:33 -0300

    aula 26 upload de images

--- ablog/settings.py
+++ ablog/settings.py
@@ -121,6 +121,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL= '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+    
+    os.path.join(BASE_DIR, 'static'),
+    
+)
 
 
 LOGIN_REDIRECT_URL =  'home'
