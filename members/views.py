30aeb6415ac042f72cfd562dab90a0f7d0a5e8a1
Vladimir <vladimirmaciel@gmail.com>
2021-08-19 20:57:48 -0300

    aula 20  create a blog profile page

--- members/views.py
+++ members/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from theblog.models import Profile
+from django.shortcuts import  render, get_object_or_404
 from django.views import generic
+from django.views.generic import DetailView
 from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm, UserChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
+# from theblog.models import Profile
+
+class ShowProfilePageView(DetailView):
+	model = Profile
+	template_name = 'registration/user_profile.html'
+
+	def get_context_data(self, *args, **kwargs):
+		#users = Profile.objects.all()
+		context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+		
+		page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+
+		context["page_user"] = page_user
+		return context
 
 
 class PasswordsChangeView(PasswordChangeView):
@@ -11,21 +27,22 @@ class PasswordsChangeView(PasswordChangeView):
     # from_class = PasswordChangeForm
     # success_url = reverse_lazy('home')
     success_url = reverse_lazy('password_success')
-    
-    
+
+
 def password_success(request):
     return render(request, 'registration/password_success.html', {})
-    
+
 
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
+
 class UserEditView(generic.UpdateView):
     form_class = EditProfileForm
     template_name = 'registration/edit_profile.html'
     success_url = reverse_lazy('home')
-    
+
     def get_object(self):
         return self.request.user
