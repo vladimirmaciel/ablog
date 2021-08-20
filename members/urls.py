c078ec559a7977dc650148c0249a64c7c13fb86f
Vladimir <vladimirmaciel@gmail.com>
2021-08-19 23:58:51 -0300

    profile account creation

--- members/urls.py
+++ members/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView,EditProfilePageView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView, EditProfilePageView, CreateProfilePageView
 # from django.contrib.auth import views as auth_views
 from . import views
 
@@ -9,12 +9,16 @@ urlpatterns = [
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
     # path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html'))
     path('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html')),
-    path('password_success', views.password_success, name="password_success"), 
-    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page'),
-    path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name='edit_profile_page'),
-   
-    
-    
+    path('password_success', views.password_success, name="password_success"),
+    path('<int:pk>/profile/', ShowProfilePageView.as_view(),
+         name='show_profile_page'),
+    path('<int:pk>/edit_profile_page/',
+         EditProfilePageView.as_view(), name='edit_profile_page'),
+    path('create_profile_page/', CreateProfilePageView.as_view(),
+         name='create_profile_page'),
+
+
+
 
 
 
